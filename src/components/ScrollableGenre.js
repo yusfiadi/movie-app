f87f78022a80f42e98ca143acd7193d7626e3986
Yusfi Adilaksa <yadilaksa@gmail.com>
2021-01-17 14:56:01 +0700

    add scrollablegenre

---
+++ src/components/ScrollableGenre.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Chip from '@material-ui/core/Chip';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+  },
+  chip: {
+    textTransform: 'none',
+    marginRight: 20,
+    fontSize: 16
+  }
+}));
+
+export default function ScrollableGenre() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default" style={{ backgroundColor: '#ffffff', boxShadow: 'none' }}>
+        <Tabs
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          <Chip label="Action" variant="outlined"  className={classes.chip} />
+          <Chip label="Crime" variant="outlined"  className={classes.chip} />
+          <Chip label="Comedy" variant="outlined" className={classes.chip} />
+          <Chip label="Drama" variant="outlined"  className={classes.chip} />
+          <Chip label="Horror" variant="outlined"  className={classes.chip} />
+        </Tabs>
+      </AppBar>
+    </div>
+  );
+}
