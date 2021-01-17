76ddacbd276de58f7004fbcc460d60c3e3099e9f
Yusfi Adilaksa <yadilaksa@gmail.com>
2021-01-17 14:56:44 +0700

    edit scrollable menu

--- src/components/ScrollableMenu.js
+++ src/components/ScrollableMenu.js
@@ -1,44 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`scrollable-auto-tabpanel-${index}`}
-      aria-labelledby={`scrollable-auto-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `scrollable-auto-tab-${index}`,
-    'aria-controls': `scrollable-auto-tabpanel-${index}`,
-  };
-}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -72,9 +36,9 @@ export default function ScrollableMenu() {
           scrollButtons="auto"
           aria-label="scrollable auto tabs example"
         >
-          <Tab label="In Theater" {...a11yProps(0)} className={classes.tab} />
-          <Tab label="Box Office" {...a11yProps(1)} className={classes.tab} />
-          <Tab label="Coming Soon" {...a11yProps(2)} className={classes.tab} />
+          <Tab label="In Theater" className={classes.tab} />
+          <Tab label="Box Office" className={classes.tab} />
+          <Tab label="Coming Soon" className={classes.tab} />
         </Tabs>
       </AppBar>
       {/* <TabPanel value={value} index={0}>
