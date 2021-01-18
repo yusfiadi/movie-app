2b2eae1df35ed768a8f03b8bf0d72fe26574de9f
Yusfi Adilaksa <yadilaksa@gmail.com>
2021-01-18 21:56:05 +0700

    touchup code

--- src/components/PlotSummary.js
+++ src/components/PlotSummary.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginBottom: 40
+  },
+  plotSummary: {
+    opacity: 0.5
+  }
+}));
 
 const PlotSummary = () => {
+  const classes = useStyles()
+
   return (
-    <div style={{ marginBottom: 40}}>
+    <div className={classes.root}>
       <Typography variant="h5" align='left' gutterBottom>
         Plot Summary
       </Typography>
-      <Typography variant="body1" gutterBottom align='left' style={{ opacity: 0.5 }}>
+      <Typography variant="body1" gutterBottom align='left' className={classes.plotSummary}>
         American car designer Carroll Shelby and driver Kn Miles battle corporate interference and the laws of physics to build a revolutionary race car for Ford in order.
       </Typography>
     </div>
