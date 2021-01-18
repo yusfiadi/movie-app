40f36cbf21dbdf5a750669c0e128875b220a280a
Yusfi Adilaksa <yadilaksa@gmail.com>
2021-01-18 19:16:48 +0700

    edit for temporary render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from './pages/Home'
+import MovieDetails from './pages/MovieDetails'
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Home />
+      <MovieDetails />
     </div>
   );
 }
