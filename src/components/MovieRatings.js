bfb8103abde6cd3b1bf0a2cdab39d0a9012bab8c
Yusfi Adilaksa <yadilaksa@gmail.com>
2021-01-18 21:52:25 +0700

    touchup code

--- src/components/MovieRatings.js
+++ src/components/MovieRatings.js
@@ -8,39 +8,72 @@ import StarBorderIcon from '@material-ui/icons/StarBorder';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    height: 100, 
+    width: '92%', 
+    backgroundColor: '#F6F6F6', 
+    borderBottomLeftRadius: 60, 
+    borderTopLeftRadius: 60, 
+    right: 0, 
+    marginLeft: 'auto', 
+    marginTop: -50, 
+    boxShadow: '0px 50px 282px #9E9E9E', 
+    overflowX: 'hidden', 
+    marginBottom: 45
   },
+  totalRatings: {
+    opacity: 0.4,
+    fontSize: 12
+  },
+  maxRating: {
+    fontSize: 12
+  },
+  starIcon: {
+    color: '#FCC419'
+  },
+  gridContainer: {
+    paddingLeft: 20, 
+    paddingTop: 13
+  },
+  boxMetascore: {
+    backgroundColor: '#51CF66', 
+    width: 28, 
+    height: 24, 
+    marginBottom: 4
+  },
+  textMetaScore: {
+    color: 'white', 
+    margin: 0, 
+  }
 }));
 
 const MovieRatings = () => {
   const classes = useStyles()
 
   return (
-    <div style={{ marginBottom: 45 }}>
-      <div className={classes.root} style={{ height: 100, width: '92%', backgroundColor: '#F6F6F6', borderBottomLeftRadius: 60, borderTopLeftRadius: 60, right: 0, marginLeft: 'auto', marginTop: -50, boxShadow: '0px 50px 282px #9E9E9E', overflowX: 'hidden' }}>
-        <Grid container spacing={1} style={{ paddingLeft: 20, paddingTop: 13, }} justify="center" >
-          <Grid item xs>
-            <StarIcon style={{ color: '#FCC419' }} />
-            <br />
-            <span>8.2</span><span style={{ fontSize: 12 }}>/10</span> <br />
-            <span style={{ opacity: 0.4, fontSize: 12 }}>150,212</span>
-          </Grid>
-          <Grid item xs>
-            <StarBorderIcon /> <br />
-            <span>Rate This</span>
-          </Grid>
-          <Grid item xs>
-            <Box display="flex" justifyContent='center'>
-              <div style={{ backgroundColor: '#51CF66', width: 28, height: 24, marginBottom: 4,}}>
-                <p style={{ color: 'white', margin: 0, }}>86</p>
-              </div>
-            </Box>
-
-            <span>Metascore</span> <br />
-            <span style={{ opacity: 0.4, fontSize: 12 }}>62 critic reviews</span>
-          </Grid>
+    <div className={classes.root}>
+      <Grid container spacing={1} className={classes.gridContainer} justify="center" >
+        <Grid item xs>
+          <StarIcon className={classes.starIcon} />
+          <br />
+          <span>8.2</span><span className={classes.maxRating}>/10</span> <br />
+          <span className={classes.totalRatings}>150,212</span>
         </Grid>
-      </div>
-    </div >
+        <Grid item xs>
+          <StarBorderIcon /> <br />
+          <span>Rate This</span>
+        </Grid>
+        <Grid item xs>
+          <Box display="flex" justifyContent='center'>
+            <Box className={classes.boxMetascore}>
+              <p className={classes.textMetaScore}>86</p>
+            </Box>
+          </Box>
+
+          <span>Metascore</span> <br />
+          <span className={classes.totalRatings}>62 critic reviews</span>
+        </Grid>
+      </Grid>
+    </div>
   )
 }
 
