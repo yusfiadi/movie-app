7f6e54390eca7cd194869e2d51381b4557535fc7
Yusfi Adilaksa <yadilaksa@gmail.com>
2021-01-18 15:42:13 +0700

    create new component for movie image

---
+++ src/components/MovieImage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import FordVFerrari from '../assets/images/ford-v-ferrari2.png'
+
+const MovieImage = () => {
+  return (
+    <div>
+      <div style={{ height: 300, width : '100%', backgroundColor: 'gray', borderBottomLeftRadius: 60}}>
+        {/* <img src={FordVFerrari} alt='Movie' style={{ height: 300, width: '100%', borderBottomLeftRadius: 60 }} /> */}
+      </div>
+    </div>
+  )
+}
+
+export default MovieImage
