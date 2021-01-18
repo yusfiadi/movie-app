45a527580aac11532edc19556fd36ebdca7d5c75
Yusfi Adilaksa <yadilaksa@gmail.com>
2021-01-18 15:42:53 +0700

    create new component for movie ratings

---
+++ src/components/MovieRatings.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+const MovieRatings = () => {
+  const classes = useStyles()
+
+  return (
+    <div>
+      <div className={classes.root} style={{ height: 100, width: '90%', backgroundColor: '#F6F6F6', borderBottomLeftRadius: 60, borderTopLeftRadius: 60, right: 0, position: 'absolute', top: 250, boxShadow: '0px 50px 282px #9E9E9E', overflowX: 'hidden' }}>
+        <Grid container spacing={1} style={{ paddingLeft: 30, paddingTop: 13, }} justify="center" >
+          <Grid item xs>
+            <StarIcon style={{ color: '#FCC419' }} />
+            <br />
+            <span>8.2</span><span style={{ fontSize: 12 }}>/10</span> <br />
+            <span style={{ opacity: 0.4, fontSize: 12 }}>150,212</span>
+          </Grid>
+          <Grid item xs>
+            <StarBorderIcon /> <br />
+            <span>Rate This</span>
+          </Grid>
+          <Grid item xs>
+            <Box display="flex" justifyContent='center'>
+              <div style={{ backgroundColor: '#51CF66', width: 28, height: 24, marginBottom: 4,}}>
+                <p style={{ color: 'white', margin: 0, }}>86</p>
+              </div>
+            </Box>
+
+            <span>Metascore</span> <br />
+            <span style={{ opacity: 0.4, fontSize: 12 }}>62 critic reviews</span>
+          </Grid>
+        </Grid>
+      </div>
+    </div >
+  )
+}
+
+export default MovieRatings
