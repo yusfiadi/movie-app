eeff3edf52918f0536e05191a78b490595f55c1a
Yusfi Adilaksa <yadilaksa@gmail.com>
2021-01-18 15:42:33 +0700

    create new component for movie info

---
+++ src/components/MovieInfo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography';
+
+const MovieInfo = () => {
+  return (
+    <div style={{marginTop: 100}}>
+      <Typography variant="h4" gutterBottom>
+        Ford v Ferrari
+      </Typography>
+    </div>
+  )
+}
+
+export default MovieInfo
